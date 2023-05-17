d299e0cb93605b753fcd9005d40d2e77b878f3ef
salma-s0liman <130157796+salma-s0liman@users.noreply.github.com>
2023-05-17 13:48:03 +0300

    Update LogInToAccount.java

--- LogInToAccount.java
+++ LogInToAccount.java
@@ -1,68 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bankingsystem;
 
-/**
- *
- * @author Time Computer
- */
-public class LogInToAccount implements Service{
+package atm;
+import java.util.*;
+
+
+public class Account extends Registration implements Service{
+     
+   
     
-
-   private int balance=10000;
-    private String password="1234 ";
-    boolean flag=true;
+    public void deposit(int dep){
+        this.balance = this.balance + dep;
+        System.out.println(dep + " Deposited.");
+    }    
     
-    public double getBalance() {
-        return balance;
+    public void withDraw(int with){
+        Scanner s = new Scanner(System.in);
+        while(with>this.balance){
+            System.out.println("Insufficient Balance.\nPlease Insert Another Number :  ");
+                with = s.nextInt();
+        }        
+            this.balance = this.balance - with;
+            System.out.println(with + " Withdrown.");
+        
+    }
+    
+    public void checkBalance(){
+        System.out.print("your Balance is : " + this.balance);
     }
 
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-  
-    @Override
-    public void checkpassword(String password) {
-       if(this.password.equals(password)){
-           System.out.println("The password is correct");
-           this.flag=false;
-       }  
-       else{
-           System.out.println("The password is wrong , please try again!");
-       }
-    }
-
-    @Override
-    public double withDraw(double WithdrawalAmount) {
-        if(WithdrawalAmount<=this.balance){
-        this.balance-=WithdrawalAmount;
-        System.out.println("Your amount is withdrawn");}
- 
-        else{
-        System.out.println("The balance is not enough!");}
-        return this.balance;
-    }
-
-    @Override
-    public double deposit(double depositAmount) {
-        this.balance+=depositAmount;
-        System.out.println("Your amount is deposited");
-        return this.balance;    }
-
-    @Override
-    public double checkBalance() {
-    return this.balance;
-            }
-    }
+   
+    
+    
+}
